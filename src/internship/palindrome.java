7af05394fbbdd6f444c7bb872696eb1b9cdd3d31
Ritikarajwar <ritikarajwar0@gmail.com>
2024-09-28 20:25:45 +0530

    first commit

---
+++ src/internship/palindrome.java
@@ -0,0 +1,52 @@
+package internship;
+
+import java.util.Scanner;
+
+class palindrome {
+    public boolean isPalindrome(String s) {
+       
+        if (s.isEmpty()) {
+            return true;
+        }
+        
+        int start = 0;
+        int last = s.length() - 1;
+
+        while (start <= last) {
+            char currFirst = s.charAt(start);
+            char currLast = s.charAt(last);
+             
+            if (!Character.isLetterOrDigit(currFirst)) {
+                start++;
+            } 
+            
+            else if (!Character.isLetterOrDigit(currLast)) {
+                last--;
+            } 
+            
+            else {
+                if (Character.toLowerCase(currFirst) != Character.toLowerCase(currLast)) {
+                    return false;
+                }
+                start++;
+                last--;
+            }
+        }
+        
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        palindrome sol = new palindrome();
+        
+        System.out.println("Enter a word or phrase to check if it's a palindrome: ");
+        String userInput = sc.nextLine();
+        
+        if (sol.isPalindrome(userInput)) {
+            System.out.println("The input is a palindrome.");
+        } else {
+            System.out.println("The input is not a palindrome.");
+        }
+    }
+}
+
